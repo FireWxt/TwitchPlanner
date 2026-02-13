251eea683f898c65e41273cdd412e9e05c2f426f
Simon Daconceicao Semiao <semiaosimon@gmail.com>
2026-02-13 02:10:21 +0100

    Adjusting back to db var

--- backend/api/profile.js
+++ backend/api/profile.js
@@ -16,15 +16,26 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-// POST /api/users/me/avatar
+
+// GET /api/profile/me
+router.get("/me", requireAuth, async (req, res) => {
+  const [rows] = await db.execute(
+    `SELECT Id_USER, email, twitch_url,avatar_url FROM USER_ WHERE Id_USER = ? LIMIT 1`,
+    [req.user.Id_USER]
+  );
+
+  if (!rows.length) return res.status(404).json({ error: "Utilisateur introuvable" });
+  res.json(rows[0]);
+});
+
 router.post("/me/avatar", requireAuth, upload.single("avatar"), async (req, res) => {
   if (!req.file) return res.status(400).json({ error: "Fichier manquant" });
 
   const avatarUrl = `/uploads/avatars/${req.file.filename}`;
 
   await db.execute(
-    `UPDATE utilisateurs SET avatar_url = ? WHERE id = ?`,
-    [avatarUrl, req.user.id]
+    `UPDATE USER_ SET avatar_url = ? WHERE Id_USER = ?`,
+    [avatarUrl, req.user.Id_USER]
   );
 
   res.json({ avatar_url: avatarUrl });
